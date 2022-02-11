3d3effaea535d562108414b200c2a741f567ad9b
YANH216 <65383837+YANH216@users.noreply.github.com>
2022-02-11 22:34:31 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import { Route, Routes, Navigate } from 'react-router-dom'
+import Admin from './pages/admin/Admin'
+import Login from './pages/Login/Login'
+ 
 export default function App() {
     return (
-        <div>
-            App...
-        </div>
+        <>
+            <Routes>
+                <Route path= '/*' element= { <Admin/> } />
+                <Route path= '/login' element= { <Login/> } />
+                <Route index element= { <Navigate to= '/login'/> }/>
+            </Routes>
+        </>
     )
 }
